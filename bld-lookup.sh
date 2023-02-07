bf49e19c378d03a5a5d9a56e2231a78980f6735e
Yevgeniy Goncharov <g.yevgeniy.p@gmail.com>
2023-02-07 17:54:30 +0600

    + working with list

--- bld-lookup.sh
+++ bld-lookup.sh
@@ -8,6 +8,48 @@
 PATH=$PATH:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
 SCRIPT_PATH=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)
 
+# Args and Vars
+# ---------------------------------------------------\
+
+usage() {
+    echo -e "\nUse script with arguments:
+    -d <checking domain name>
+    -l <path to domain resolvers list>
+    "
+    exit 0
+}
+
+# Checks arguments
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -d|--domain) _DOMAIN=1 _DOMAIN_DATA=$2; ;;
+        -l|--list) _LIST=1 _LIST_DATA=$2; shift ;;
+        # -h|--help) usage ;; 
+        # *) usage ;;
+    esac
+    shift
+done
+
+if [[ "$_DOMAIN" -eq "1" ]]; then
+    if [[ -z "$_DOMAIN_DATA" ]]; then
+        usage
+    fi
+    RESOLVE="$_DOMAIN_DATA"
+else
+    usage
+fi
+
+if [[ "$_LIST" -eq "1" ]]; then
+    if [[ -z "$_LIST_DATA" ]]; then
+        echo "Set custom list please. Exit. Bye."
+        exit 0
+    fi
+    DOMAINs=`cat $_LIST_DATA`
+else
+    # Domain max days expires
+    DOMAINs=`cat $SCRIPT_PATH/domains.txt`
+fi
+
 # Initial variables
 # ---------------------------------------------------\
 Info() {
@@ -20,26 +62,32 @@ space() {
 
 # Checking arguments
 # ---------------------------------------------------\
-if [[ $# -eq 0 ]] ; then
-    echo 'Set domain name for resolving. Exit.'
-    exit 1
-else
-    RESOLVE=$1
-fi
+# if [[ -z "$1" ]] ; then
+#     # echo 'Set domain name for resolving. Exit.'
+#     usage
+#     exit 1
+# else
+#     RESOLVE=$1
+# fi
 
 # Actions
 # ---------------------------------------------------\
-ips=$(dig +short bld.sys-adm.in)
+for d in ${DOMAINs}; do
 
-# echo -e "\n----------------------- Starting DNS resolving -----------------------\n"
+    Info "\n----------------------- Working with domain name: $d -----------------------"
+    ips=$(dig +short bld.sys-adm.in)
 
-for ip in ${ips}; do
+    for ip in ${ips}; do
 
-    Info "\n----------------------- Starting from IP: $ip -----------------------\n"
-    nslookup $RESOLVE $ip
+        Info "\n----------------------- IP - $ip:\n"
+        nslookup $RESOLVE $ip
 
-    time=`dig @$ip $RESOLVE | awk '/Query time:/ {print " "$4}'`
-    echo -e "Response speed: $time ms"
+        time=`dig @$ip $RESOLVE | awk '/Query time:/ {print " "$4}'`
+        echo -e "Response speed: $time ms"
 
-    # echo -e "\n----------------------- Done from IP: $ip -----------------------\n"
+        # echo -e "\n----------------------- Done from IP: $ip -----------------------\n"
+    done
+
 done
+
+
